d1906a70eaf2e1f3db410caf70885e326dd08da8
ayushmall0710 <mall.ayush0710@gmail.com>
2020-09-04 17:10:35 +0530

    changes in time

--- SITE/script.js
+++ SITE/script.js
@@ -1,16 +1,17 @@
 
-function changing_milestone(to_time){
-  var videoPlayer= document.getElementById('my_vid')
+function changing_milestone(to_time) {
+  var videoPlayer = document.getElementById('my_vid')
   videoPlayer.currentTime = to_time;
+  videoPlayer.play();
 }
 
 document.addEventListener('keyup', event => {
-  var videoPlayer= document.getElementById('my_vid')
-   if (event.code === 'Space') {
-     if (videoPlayer.paused == false) {
-       videoPlayer.pause();
-     } else {
-       videoPlayer.play();
-     }
-   }
+  var videoPlayer = document.getElementById('my_vid')
+  if (event.code === 'Space') {
+    if (videoPlayer.paused == false) {
+      videoPlayer.pause();
+    } else {
+      videoPlayer.play();
+    }
+  }
 });
