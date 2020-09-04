0823c93dcfacbd3cdb1569ff3bf2beaa1572caf0
Rahul Unadkat <unadkartrahulb@gmail.com>
2020-09-04 17:03:17 +0530

    Created Javascript file

---
+++ SITE/script.js
@@ -0,0 +1,16 @@
+
+function changing_milestone(to_time){
+  var videoPlayer= document.getElementById('my_vid')
+  videoPlayer.currentTime = to_time;
+}
+
+document.addEventListener('keyup', event => {
+  var videoPlayer= document.getElementById('my_vid')
+   if (event.code === 'Space') {
+     if (videoPlayer.paused == false) {
+       videoPlayer.pause();
+     } else {
+       videoPlayer.play();
+     }
+   }
+});
