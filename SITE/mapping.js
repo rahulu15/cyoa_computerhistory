8e802bd7c5e7e6fbf6250dfcdae4c2ef87d7c2d5
ayushmall0710 <mall.ayush0710@gmail.com>
2020-09-11 16:21:34 +0530

    Added Object for mappig and Arrow key binding

---
+++ SITE/mapping.js
@@ -0,0 +1,32 @@
+mapping = {
+    "intro": {
+        "name": "Introduction",
+        "previous": 0,
+        "start": 0,
+        "next": 45
+    },
+    "milestone_1": {
+        "name": "First Milestone",
+        "previous": 45,
+        "start": 45,
+        "next": 101.9
+    },
+    "milestone_2": {
+        "name": "Second Milestone",
+        "previous": 45,
+        "start": 101.9,
+        "next": 208.6
+    },
+    "milestone_3": {
+        "name": "Third Milestone",
+        "previous": 101.9,
+        "start": 208.6,
+        "next": 295.2
+    },
+    "milestone_4": {
+        "name": "Fourth Milestone",
+        "previous": 208.6,
+        "start": 295.2,
+        "next": 415
+    }
+}
--- SITE/script.js
+++ SITE/script.js
@@ -5,6 +5,17 @@ function changing_milestone(to_time) {
   videoPlayer.play();
 }
 
+function changing_milestone_json(element) {
+  var id = element.id;
+  var map = mapping
+  console.log(id)
+  console.log(map)
+  var videoPlayer = document.getElementById('our_vid')
+  videoPlayer.currentTime = map[id].start;
+  console.log(map[id].start)
+  videoPlayer.play();
+}
+
 document.addEventListener('keyup', event => {
   var videoPlayer = document.getElementById("our_vid")
   if (event.code === 'Space') {
@@ -16,11 +27,35 @@ document.addEventListener('keyup', event => {
   }
 });
 
-function click_to_flip(){
+document.addEventListener('keyup', event => {
+  var videoPlayer = document.getElementById("our_vid")
+  if (event.code === 'ArrowRight') {
+    for (const milestone in mapping) {
+      if (videoPlayer.currentTime > mapping[milestone].start && videoPlayer.currentTime < mapping[milestone].next) {
+        videoPlayer.currentTime = mapping[milestone].next;
+        break;
+      }
+    }
+  }
+});
+
+document.addEventListener('keyup', event => {
+  var videoPlayer = document.getElementById("our_vid")
+  if (event.code === 'ArrowLeft') {
+    for (const milestone in mapping) {
+      if (videoPlayer.currentTime > mapping[milestone].start && videoPlayer.currentTime < mapping[milestone].next) {
+        videoPlayer.currentTime = mapping[milestone].previous;
+        break;
+      }
+    }
+  }
+});
+
+function click_to_flip() {
   const card_container = document.querySelector(".flip-card-container")
   card_container.addEventListener('click', toggle_flip());
 
-  function toggle_flip(){
+  function toggle_flip() {
     card_container.classList.toggle("flipit");
   }
 }
