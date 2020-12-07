73ef5ad5304c59a77b857dd2cb9ba6818377de10
Rahul Unadkat <unadkartrahulb@gmail.com>
2020-12-08 00:37:48 +0530

    Added buttons

--- SITE/mapping.js
+++ SITE/mapping.js
@@ -28,5 +28,49 @@ mapping = {
         "previous": 208.6,
         "start": 295.2,
         "next": 415
+    },
+
+    "milestone_5": {
+        "name": "Fifth Milestone",
+        "previous": 295.2,
+        "start": 415,
+        "next": 421
+    },
+    "milestone_6": {
+        "name": "Sixth Milestone",
+        "previous": 415,
+        "start": 421,
+        "next": 427
+    },
+    "milestone_7": {
+        "name": "Seventh Milestone",
+        "previous": 421,
+        "start": 427,
+        "next": 430
+    },
+    "milestone_8": {
+        "name": "Eighth Milestone",
+        "previous": 427,
+        "start": 430,
+        "next": 435
+    },
+    "milestone_9": {
+        "name": "Nineth Milestone",
+        "previous": 430,
+        "start": 435,
+        "next": 440
+    },
+    "milestone_10": {
+        "name": "Tenth Milestone",
+        "previous": 430,
+        "start": 440,
+        "next": 445
+    },
+
+    "milestone_11": {
+        "name": "Fourth Milestone",
+        "previous": 440,
+        "start": 445,
+        "next": 450
     }
 }
--- SITE/script.js
+++ SITE/script.js
@@ -1,15 +1,25 @@
+// function to change the milestone on clicking the buttons
 
 function changing_milestone(element) {
   var id = element.id;
   var map = mapping
   console.log(id)
   console.log(map)
+// seeking the current time to milestone time
   var videoPlayer = document.getElementById('our_vid')
   videoPlayer.currentTime = map[id].start;
   console.log(map[id].start)
-  videoPlayer.play();
-}
 
+  if (videoPlayer.paused == false) {
+    videoPlayer.pause();
+    setTimeout(()=> videoPlayer.play(),500);
+  } else {
+    setTimeout(()=> videoPlayer.play(),500);
+  }
+
+
+}
+//spacebar to pause and play
 document.addEventListener('keyup', event => {
   var videoPlayer = document.getElementById("our_vid")
   if (event.code === 'Space') {
@@ -20,7 +30,7 @@ document.addEventListener('keyup', event => {
     }
   }
 });
-
+//right arrow key to jump to next
 document.addEventListener('keyup', event => {
   var videoPlayer = document.getElementById("our_vid")
   if (event.code === 'ArrowRight') {
@@ -32,7 +42,7 @@ document.addEventListener('keyup', event => {
     }
   }
 });
-
+// left arrow key to jump to previous
 document.addEventListener('keyup', event => {
   var videoPlayer = document.getElementById("our_vid")
   if (event.code === 'ArrowLeft') {
@@ -45,13 +55,14 @@ document.addEventListener('keyup', event => {
   }
 });
 
+//flipping the card on clicking the power button
 function click_to_flip() {
   const card_container = document.querySelector(".flip-card-container")
   card_container.addEventListener('click', toggle_flip());
   function toggle_flip() {
     card_container.classList.toggle("flipit");
   }
-
+//rotating and sliding the power button on click
   const pow_btn = document.querySelector(".power-buttons")
   pow_btn.addEventListener('click', toggle_rotate());
   function toggle_rotate(){
@@ -59,6 +70,9 @@ function click_to_flip() {
     var videoPlayer = document.getElementById("our_vid")
     if (videoPlayer.paused == false) {
       videoPlayer.pause();
+      setTimeout(()=> videoPlayer.currentTime=0,1000);
+    }
+    else{
       setTimeout(()=> videoPlayer.currentTime=0,1000);
     }
   }
